54199b49a684325dda072ee5d1fcba20746030be
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-10-26 12:43:47 -0300

    include annotation in partService

--- backend/src/main/java/br/com/desafio/rav/partsSystem/services/PartService.java
+++ backend/src/main/java/br/com/desafio/rav/partsSystem/services/PartService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.desafio.rav.partsSystem.entities.Part;
 import br.com.desafio.rav.partsSystem.repositories.PartRepository;
@@ -14,6 +15,7 @@ public class PartService {
 	@Autowired
 	private PartRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Part> findAll(){
 		return repository.findAll();
 	}
