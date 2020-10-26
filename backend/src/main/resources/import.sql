efe6a4b09f505fb52cba882cad28a979e8816413
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-10-26 12:59:40 -0300

    Include seeding in data base

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_part (name, weight, price, type) VALUES ('peça1', 300.00, 100.00, 'Geral');
+INSERT INTO tb_part (name, weight, price, type) VALUES ('peça2', 300.00, 100.00, 'Geral');
+INSERT INTO tb_part (name, weight, price, type) VALUES ('peça3', 300.00, 100.00, 'Geral');
+INSERT INTO tb_part (name, weight, price, type) VALUES ('peça4', 300.00, 100.00, 'Geral');
+INSERT INTO tb_part (name, weight, price, type) VALUES ('peça5', 300.00, 100.00, 'Geral');
