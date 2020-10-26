f123bf7531aecd62965532916126a40de6a3b0e5
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-10-26 11:02:29 -0300

    create setup backend project

---
+++ backend/src/main/java/br/com/desafio/rav/partsSystem/PartsSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.rav.partsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartsSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/desafio/rav/partsSystem/PartsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.rav.partsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
