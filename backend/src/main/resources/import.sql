12a180b02d537957014519a1ac4abbaea2d18d57
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-10-28 15:20:47 -0300

    refactor file import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,11 @@
-INSERT INTO tb_part (name, weight, price, type) VALUES ('peça1', 300.00, 100.00, 'Geral');
-INSERT INTO tb_part (name, weight, price, type) VALUES ('peça2', 300.00, 100.00, 'Geral');
-INSERT INTO tb_part (name, weight, price, type) VALUES ('peça3', 300.00, 100.00, 'Geral');
-INSERT INTO tb_part (name, weight, price, type) VALUES ('peça4', 300.00, 100.00, 'Geral');
-INSERT INTO tb_part (name, weight, price, type) VALUES ('peça5', 300.00, 100.00, 'Geral');
+INSERT INTO tb_simulation (name, description) VALUES ('Simulação 1','essa eh uma simulação teste');
+
+INSERT INTO tb_part (name, weight, price, type, simulation_id) VALUES ('peça1', 300.00, 100.00, 'Geral', 1);
+INSERT INTO tb_part (name, weight, price, type, simulation_id) VALUES ('peça2', 300.00, 100.00, 'Geral', 1);
+
+INSERT INTO tb_part_child (name, weight, price, type, part_id) VALUES ('peça filha1', 300.00, 100.00, 'interior', 1);
+INSERT INTO tb_part_child (name, weight, price, type, part_id) VALUES ('peça filha1', 300.00, 100.00, 'interior', 1);
+INSERT INTO tb_part_child (name, weight, price, type, part_id) VALUES ('peça filha1', 300.00, 100.00, 'interior', 2);
+
+
+
